791de7575ff73ad7baf7a753d44d2061351afc8f
Ujjwal <ujjwalchand183@gmail.com>
2024-10-06 03:08:46 +0530

    9 and Activity LifeCycle

---
+++ 14_Toggle_Button/app/src/main/java/com/example/togglebutton/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.togglebutton;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ToggleButton toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        toggleButton = findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean check) {
+                if (check) Toast.makeText(getApplicationContext(),"Toggle is Enabled",Toast.LENGTH_SHORT).show();
+                else Toast.makeText(getApplicationContext(),"Toggle is Disabled",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- 6_Activity_Life_Cycle/app/src/main/java/com/example/lifecycle/MainActivity.java
+++ 6_Activity_Life_Cycle/app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lifecycle;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.d("LifeCycle","onCreate invoked");
+        }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Lifecycle","onStart invoked");
+    }
+    protected  void onResume(){
+        super.onResume();
+        Log.d("Lifecycle","onResume invoked");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Lifecycle","onPause invoked");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Lifecycle","onStop invoked");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Lifecycle","onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle","onDestroy invoked");
     }
 }
