7f20c15177b353001f5e9731f1a6d7717138d43d
Ujjwal <ujjwalchand183@gmail.com>
2024-10-06 16:07:34 +0530

    Updated the layout Change App

--- 13_Login/app/src/main/java/com/example/form/MainActivity.java
+++ 13_Login/app/src/main/java/com/example/form/MainActivity.java
@@ -1,11 +1,6 @@
-package com.example.changelayout;
+package com.example.form;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-    RelativeLayout main;
-    Button change;
-    Random rnd = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +20,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        main = findViewById(R.id.main);
-        change = findViewById(R.id.change);
-        change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                int color = Color.argb(255,rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256));
-                main.setBackgroundColor(color);
-            }
-        });
-
     }
 }
