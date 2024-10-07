78b385fbadd783ee245a2082a7e6565e97aae87e
Ujjwal <ujjwalchand183@gmail.com>
2024-10-07 15:24:22 +0530

    new File

---
+++ Practice/Lamp/app/src/main/java/com/example/lamp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.lamp;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView image;
+    Switch on;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        image = findViewById(R.id.image);
+        on = findViewById(R.id.on);
+        on.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    image.setImageResource(R.drawable.on);
+                }
+                else image.setImageResource(R.drawable.off);
+            }
+        });
+    }
+}
